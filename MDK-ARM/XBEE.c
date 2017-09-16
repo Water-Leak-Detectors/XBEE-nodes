6ac24149130064282dce1edb59b24226e07b6365
mhtb32 <mhtb32@gmail.com>
2017-09-16 11:21:00 +0430

    Xbee frame is added

--- MDK-ARM/XBEE.c
+++ MDK-ARM/XBEE.c
@@ -1,1 +1,19 @@
 #include "XBEE.h"
+
+/* Function Definitions----------------------------------------*/
+/**
+  * @brief  This function calculates checksum and puts it in your frame
+	* @param  frame: pointer to xbee frame
+	*	@paran	dataptr: pointer to sending data
+	* @param 	size: size of sending data
+  * @retval None
+  */
+void calc_checksum(xbee_frame* frame, uint8_t* dataptr, uint16_t size)
+{
+	uint16_t cnt=0;
+	frame->checksum = 0;
+	for(cnt=0 ; cnt < size ; cnt++)
+	{
+		frame->checksum += dataptr[cnt];
+	}
+}
--- MDK-ARM/XBEE.h
+++ MDK-ARM/XBEE.h
@@ -1,8 +1,29 @@
+/************************
+Developer: Mohammad Hussein Tavakoli Bina
+*/
+
 #ifndef XBEE_H
 #define XBEE_H
 
+/* Includes----------------------------------------*/
+#include "stdint.h"
 
+/* Variables---------------------------------------*/
+const uint8_t trans_req_ID = 0x10;
+const uint8_t modem_stat = 0x8A;
+const uint8_t trans_stat = 0x8B;
 
+/* Typedefs----------------------------------------*/
+typedef struct
+{
+	const uint8_t start_delimiter;
+	uint8_t length[2];
+	uint8_t cmdID;
+	uint8_t checksum;
+}	xbee_frame;
+
+/* Function Prototypes-----------------------------*/
+void calc_checksum(xbee_frame*, uint8_t*, uint16_t);
 
 
 #endif
